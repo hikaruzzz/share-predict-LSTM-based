ad562c15f59500f712418a1d0ac46dcc816efeff
hikaruzzz <36815102+hikaruzzz@users.noreply.github.com>
2021-01-08 01:12:17 +0800

    fix

--- GRU_torch/models.py
+++ GRU_torch/models.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 class GRUNet(nn.Module):
-    def __init__(self, input_size, predict_times_step=1):
+    def __init__(self, input_size, out_channel=1, predict_times_step=1):
         super(GRUNet, self).__init__()
         self.rnn = nn.GRU(
             input_size=input_size,
@@ -13,10 +13,10 @@ class GRUNet(nn.Module):
         self.predict_times_step = predict_times_step
         if predict_times_step == 1:
             self.out1 = nn.Sequential(
-                nn.Linear(128, 1)
+                nn.Linear(128, out_channel)
             ) # output future_time = 1
         else:
-            self.out = nn.Conv1d(128, 1, 1) # output future_time = time_step
+            self.out = nn.Conv1d(128, out_channel, 1) # output future_time = time_step
 
     def forward(self, x):
         r_out, (h_n, h_c) = self.rnn(x, None)  # None 表示 hidden state 会用全0的 state
@@ -25,6 +25,7 @@ class GRUNet(nn.Module):
             out = self.out1(r_out[:, -1])
         else:
             r_out = r_out.permute([0,2,1])
-            out = self.out(r_out).squeeze(dim=-2)[:,:self.predict_times_step]
+            # print(self.out(r_out).permute([0,2,1]).shape)
+            out = self.out(r_out).permute([0,2,1])[:,:self.predict_times_step,:]
         return out
 
